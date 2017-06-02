4458e38a69dc057e71142348890d67ba41640c62
Esref Durna <edurna@gmail.com>
2017-06-01 17:21:17 -0700

    converting lodash to es6
    converting lodash to es6

--- src/alignTableData.js
+++ src/alignTableData.js
@@ -1,15 +1,13 @@
-import _ from 'lodash';
 import stringWidth from 'string-width';
 import alignString from './alignString';
-
 /**
  * @param {table~row[]} rows
  * @param {Object} config
  * @returns {table~row[]}
  */
 export default (rows, config) => {
-  return _.map(rows, (cells) => {
-    return _.map(cells, (value, index1) => {
+  return rows.map((cells) => {
+    return cells.map((value, index1) => {
       const column = config.columns[index1];
 
       if (stringWidth(value) === column.width) {
