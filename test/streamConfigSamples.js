ce2e916b1ef50ebf28eb68c1227d75707d4f924b
Gajus Kuizinas <gajus@gajus.com>
2019-06-16 15:50:50 +0100

    fix: remove references to non-supported property minWidth (fixes #95)

--- test/streamConfigSamples.js
+++ test/streamConfigSamples.js
@@ -44,20 +44,14 @@ export default {
       columns: {
         0: {
           alignment: 'left',
-
-          // minWidth: 10,
           width: 10
         },
         1: {
           alignment: 'center',
-
-          // minWidth: 10,
           width: 10
         },
         2: {
           alignment: 'right',
-
-          // minWidth: 10,
           width: 10
         }
       }
