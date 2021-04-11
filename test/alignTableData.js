7ca33294d8a3b98e8fef9619d7c14e856875b6b4
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-04-11 15:19:49 +0700

    Add test for alignTableData

---
+++ test/alignTableData.js
@@ -0,0 +1,66 @@
+/* eslint-disable max-nested-callbacks */
+
+import {
+  expect,
+} from 'chai';
+import chalk from 'chalk';
+import alignTableData from '../src/alignTableData';
+import makeConfig from '../src/makeConfig';
+
+describe('alignTableData', () => {
+  context('when the string width is equal to column width config', () => {
+    it('returns the unchange string', () => {
+      const rows = [['aaa'], [chalk.red('bbb')]];
+
+      expect(alignTableData(rows, makeConfig(rows, {
+        columns: {
+          0: {
+            width: 3,
+          },
+        },
+      }))).to.deep.equal(rows);
+    });
+  });
+
+  context('when the string is different from the column width config', () => {
+    it('aligns cells with column width and alignment config', () => {
+      const rows = [['a', 'b', 'c'],
+        [chalk.red('a'), chalk.red('b'), chalk.red('c')]];
+
+      expect(alignTableData(rows, makeConfig(rows, {
+        columnDefault: {
+          width: 3,
+        },
+        columns: {
+          0: {
+            alignment: 'left',
+          },
+          1: {
+            alignment: 'right',
+          },
+          2: {
+            alignment: 'center',
+          },
+        },
+      }))).to.deep.equal([['a  ', '  b', ' c '], [
+        chalk.red('a') + '  ', '  ' + chalk.red('b'), ' ' + chalk.red('c') + ' ',
+      ]]);
+    });
+  });
+
+  context('when the string is longer then column width', () => {
+    it('throws an error', () => {
+      const rows = [['aaaa']];
+
+      expect(() => {
+        alignTableData(rows, makeConfig(rows, {
+          columns: {
+            0: {
+              width: 3,
+            },
+          },
+        }));
+      }).to.throw(Error, 'Subject parameter value width cannot be greater than the container width.');
+    });
+  });
+});
