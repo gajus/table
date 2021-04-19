493d13620af4181e7d04c14c7fb84459e597ef78
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-04-20 05:51:47 +0700

    Wrap word support ansi

--- src/wrapWord.ts
+++ src/wrapWord.ts
@@ -1,5 +1,33 @@
 import slice from 'slice-ansi';
 import stringWidth from 'string-width';
+import stripAnsi from 'strip-ansi';
+import wrapAnsi from 'wrap-ansi';
+
+const indexAnsi = (input: string, index: number): string => {
+  return stripAnsi(slice(input, index, index + 1));
+};
+
+const trimAnsi = (input: string): string => {
+  let startIndex = 0;
+  for (; startIndex < stringWidth(input); startIndex++) {
+    if (indexAnsi(input, startIndex) !== ' ') {
+      break;
+    }
+  }
+
+  let endIndex = stringWidth(input) - 1;
+  for (;endIndex >= 0; endIndex--) {
+    if (indexAnsi(input, endIndex) !== ' ') {
+      break;
+    }
+  }
+
+  return slice(input, startIndex, endIndex + 1);
+};
+
+const sliceWrap = (input: string, size: number): string => {
+  return wrapAnsi(input, size, {hard: true}).split('\n')[0];
+};
 
 export default (input: string, size: number): string[] => {
   let subject = input;
@@ -10,18 +38,19 @@ export default (input: string, size: number): string[] => {
   const re = new RegExp('(^.{1,' + String(size) + '}(\\s+|$))|(^.{1,' + String(size - 1) + '}(\\\\|/|_|\\.|,|;|-))');
 
   do {
-    let chunk;
+    let chunk: string;
 
-    chunk = re.exec(subject);
+    const match = re.exec(stripAnsi(subject));
 
-    if (chunk) {
-      chunk = chunk[0];
+    if (match) {
+      const firstMatch = match[0];
 
-      subject = slice(subject, stringWidth(chunk));
+      chunk = slice(subject, 0, firstMatch.length);
+      subject = slice(subject, firstMatch.length);
 
-      chunk = chunk.trim();
+      chunk = trimAnsi(chunk);
     } else {
-      chunk = slice(subject, 0, size);
+      chunk = sliceWrap(subject, size);
       subject = slice(subject, size);
     }
 
--- test/wrapWord.ts
+++ test/wrapWord.ts
@@ -3,14 +3,29 @@ import {
 } from 'chai';
 import wrapWord from '../src/wrapWord';
 
+const openRed = '\u001b[31m';
+const closeRed = '\u001b[39m';
+
+const stringToRed = (string: string) => {
+  return openRed + string + closeRed;
+};
+const arrayToRed = (array: string[]) => {
+  return array.map(stringToRed);
+};
+
 describe('wrapWord', () => {
   it('wraps a string at a nearest whitespace', () => {
     expect(wrapWord('aaa bbb', 5)).to.deep.equal(['aaa', 'bbb']);
     expect(wrapWord('a a a bbb', 5)).to.deep.equal(['a a a', 'bbb']);
+
+    expect(wrapWord(stringToRed('aaa bbb'), 5)).to.deep.equal(arrayToRed(['aaa', 'bbb']));
+    expect(wrapWord(stringToRed('a a a bbb'), 5)).to.deep.equal(arrayToRed(['a a a', 'bbb']));
   });
   context('a single word is longer than chunk size', () => {
     it('cuts the word', () => {
       expect(wrapWord('aaaaa', 2)).to.deep.equal(['aa', 'aa', 'a']);
+
+      expect(wrapWord(stringToRed('aaaaa'), 2)).to.deep.equal(arrayToRed(['aa', 'aa', 'a']));
     });
   });
   context('a long word with a special character', () => {
@@ -22,11 +37,73 @@ describe('wrapWord', () => {
       expect(wrapWord('aaa.bbb', 5)).to.deep.equal(['aaa.', 'bbb']);
       expect(wrapWord('aaa,bbb', 5)).to.deep.equal(['aaa,', 'bbb']);
       expect(wrapWord('aaa;bbb', 5)).to.deep.equal(['aaa;', 'bbb']);
+
+      expect(wrapWord(stringToRed('aaa\\bbb'), 5)).to.deep.equal(arrayToRed(['aaa\\', 'bbb']));
+      expect(wrapWord(stringToRed('aaa/bbb'), 5)).to.deep.equal(arrayToRed(['aaa/', 'bbb']));
+      expect(wrapWord(stringToRed('aaa_bbb'), 5)).to.deep.equal(arrayToRed(['aaa_', 'bbb']));
+      expect(wrapWord(stringToRed('aaa-bbb'), 5)).to.deep.equal(arrayToRed(['aaa-', 'bbb']));
+      expect(wrapWord(stringToRed('aaa.bbb'), 5)).to.deep.equal(arrayToRed(['aaa.', 'bbb']));
+      expect(wrapWord(stringToRed('aaa,bbb'), 5)).to.deep.equal(arrayToRed(['aaa,', 'bbb']));
+      expect(wrapWord(stringToRed('aaa;bbb'), 5)).to.deep.equal(arrayToRed(['aaa;', 'bbb']));
     });
   });
   context('a special character after the length of a container', () => {
     it('does not include special character', () => {
       expect(wrapWord('aa-bbbbb-cccc', 5)).to.deep.equal(['aa-', 'bbbbb', '-cccc']);
+
+      expect(wrapWord(stringToRed('aa-bbbbb-cccc'), 5)).to.deep.equal(arrayToRed(['aa-', 'bbbbb', '-cccc']));
+    });
+  });
+
+  context('mixed ansi', () => {
+    it('askdjhasdkj', () => {
+      expect(wrapWord(`${openRed}Lorem ${closeRed}ipsum dolor ${openRed}sit amet${closeRed}`, 5)).to.deep.equal([
+        `${openRed}Lorem${closeRed}`,
+        'ipsum',
+        'dolor',
+        `${openRed}sit${closeRed}`,
+        `${openRed}amet${closeRed}`,
+      ]);
+
+      expect(wrapWord(`${openRed}Lorem ${closeRed}ipsum dolor ${openRed}sit amet${closeRed}`, 11)).to.deep.equal([
+        `${openRed}Lorem ${closeRed}ipsum`,
+        `dolor ${openRed}sit${closeRed}`,
+        `${openRed}amet${closeRed}`,
+      ]);
+
+      expect(wrapWord(`${openRed}Lorem ip${closeRed}sum dolor si${openRed}t amet${closeRed}`, 5)).to.deep.equal([
+        `${openRed}Lorem${closeRed}`,
+        `${openRed}ip${closeRed}sum`,
+        'dolor',
+        `si${openRed}t${closeRed}`,
+        `${openRed}amet${closeRed}`,
+      ]);
+    });
+  });
+
+  context('multiple ansi', () => {
+    it('returns proper strings', () => {
+      const openBold = '\u001b[1m';
+      const closeBold = '\u001b[22m';
+
+      expect(wrapWord(`${openBold}${openRed}Lorem ipsum dolor sit${closeRed}${closeBold}`, 4)).to.deep.equal(
+        [
+          `${openBold}${openRed}Lore${closeRed}`,
+          `${openBold}${openRed}m${closeRed}${closeBold}`,
+          `${openBold}${openRed}ipsu${closeRed}`,
+          `${openBold}${openRed}m${closeRed}${closeBold}`,
+          `${openBold}${openRed}dolo${closeRed}`,
+          `${openBold}${openRed}r${closeRed}${closeBold}`,
+          `${openBold}${openRed}sit${closeBold}${closeRed}`],
+      );
+
+      expect(wrapWord(`${openBold}${openRed}Lorem ipsum dolor sit${closeRed}${closeBold}`, 5)).to.deep.equal(
+        [
+          `${openBold}${openRed}Lorem${closeRed}${closeBold}`,
+          `${openBold}${openRed}ipsum${closeRed}${closeBold}`,
+          `${openBold}${openRed}dolor${closeRed}${closeBold}`,
+          `${openBold}${openRed}sit${closeBold}${closeRed}`],
+      );
     });
   });
 });
